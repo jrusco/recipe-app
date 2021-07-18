e1d34447ca1d16091fa0e8e11a427a589ac68fe0
Juan Rusconi <11713137+jrusco@users.noreply.github.com>
2021-07-18 18:20:43 -0300

    Fix flake8 styling

--- app/core/admin.py
+++ app/core/admin.py
@@ -23,11 +23,11 @@ class UserAdmin(BaseUserAdmin):
     )
 
     add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('email', 'password1', 'password2')
-    }),
-)
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
